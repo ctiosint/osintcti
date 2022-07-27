2b85cd6bb2c7e484152690fc91cd4589b01d7665
ctiosint <110030543+ctiosint@users.noreply.github.com>
2022-07-27 09:54:41 +0700

    Update start.js

--- bot/start.js
+++ bot/start.js
@@ -2,6 +2,7 @@ const { InlineKeyboard } = require("grammy")
 
 const startHandler = async ( ctx, text ) => {
   let keyboard = new InlineKeyboard()
+  let text = "Untuk memilih engine gunakan /engine"
     .text("Virus Total", "virus-total")
     .text("IP Abuse", "ip-abuse").row()
     .text("Etherscan", "ether-scan")
